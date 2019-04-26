b57a0d4bf3dbf453564cb8c768b4941a029b04f2
2charibo <2c.haribo@google.com>
2019-04-27 01:24:44 +0900

    rimdjango

--- prj/settings.py
+++ prj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7s!f3u5=ir$ft3^%dr8@8y$=@s%-bs66a#lv2h%@^imz+f46a8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rimdjango1105.pythonanywhere.com']
 
 
 # Application definition
