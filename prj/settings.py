546dbf7e88591eefeb6647f80c0701df5ff6b259
rimdjango06 <rimdjango06@gmail.com>
2019-04-29 01:03:06 +0900

    Changed ...

--- prj/settings.py
+++ prj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7s!f3u5=ir$ft3^%dr8@8y$=@s%-bs66a#lv2h%@^imz+f46a8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
